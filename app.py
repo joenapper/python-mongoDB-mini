afbceb4bad7931b52af04360827ef5bc84c18d63
joenapper <joe.napper@hotmail.co.uk>
2020-07-10 13:28:00 +0000

    Building out the front end of the menu

--- app.py
+++ app.py
@@ -15,17 +15,43 @@ COLLECTION_NAME = "MyFirstMDB"
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("Mongo is connected!")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
 
-conn = mongo_connect(MONGODB_URI)
+def show_menu():
+    print("")
+    print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
 
+    option = input("Enter option: ")
+    return option
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You have selected option 1")
+        elif option == "2":
+            print("You have selected option 2")
+        elif option == "3":
+            print("You have selected option 3")
+        elif option == "4":
+            print("You have selected option 4")
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGODB_URI)
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
+main_loop()
